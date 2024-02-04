3eb6481ac654b8234be4b1a569860fdb365fd040
Anelise Barros <annebarros00@gmail.com>
2024-02-04 10:17:07 -0400

    feature: atualizando projetos

--- src/app/componentes/card/card.component.ts
+++ src/app/componentes/card/card.component.ts
@@ -3,13 +3,19 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.scss']
 })
 
 export class CardComponent implements OnInit {
 
   @Input()
   imgProjeto: string = ""
+
+  @Input()
+  tipoProjeto: string = ""
+
+  @Input()
+  linkProjeto: string = ""
 
 
   constructor() { }
